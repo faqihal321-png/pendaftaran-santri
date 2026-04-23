b4144d8bdfdd05d1d2228c21fc3fc801d8088aee
faqihal321@gmail.com <Lampung04112003>
2026-04-23 08:31:32 +0700

    fix: syntax error di console log

--- server.js
+++ server.js
@@ -211,4 +211,6 @@ app.get('/admin', checkAuth, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => { console.log(\`✅ Server Aktif di Port \${PORT}\`); });
+app.listen(PORT, '0.0.0.0', () => { 
+    console.log("✅ Server Aktif di Port " + PORT); 
+});
